c59eb3f717db1fadbc2407eb6767664f0843f458
mirza smrkovic <msmrkovic1@etf.unsa.ba>
2019-04-08 12:35:34 +0200

    zadatak nije dovrsen

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,33 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class Controller {
+    public class Controller implements Initializable {
     public TextArea unos;
     public TextArea rezultat;
     public Button dodajArtikle;
 
+    public Button obracunaj;
+    public ChoiceBox<Artikal> cbArtikli;
+    public Spinner spArtikli;
+    public TextArea artikliRacun;
+
+    private ObservableList<Artikal> observableList = FXCollections.observableArrayList();
+    private Racun racun = new Racun();
+
     private ArrayList<sample.Artikal> artikli = new ArrayList<>();
     public void dodajArtikle(ActionEvent actionEvent){
-        System.out.println("Cao");
         String[] artiklStr = unos.getText().split("\n");
 
         for(String linija: artiklStr) {
@@ -28,6 +42,37 @@ public class Controller {
             res+= artikal + "\n";
         }
 
+        observableList.clear();
+        observableList.setAll(artikli);
+//        cbArtikli.getSelectionModel().selectFirst();
+//        spArtikli.getValue()
+
+
         rezultat.setText(res);
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        observableList.setAll(artikli);
+        cbArtikli.setItems(observableList);
+    }
+
+    public void Obracunaj(ActionEvent actionEvent) {
+        Artikal trenutni = cbArtikli.getSelectionModel().getSelectedItem();
+        int kolicina = spArtikli.getValue();
+        racun.dodajStavku(trenutni, kolicina);
+
+        @Override
+        public String toString() {
+            String str = "";
+
+            for(Stavka s : stavke) {
+                str += String.format("%-8s %-4 %.2f\sn", s.artikal.getSifra(), s.kolicina, s.kolicina * s.artikal.getCijena());
+            }
+            str += String.format("%-12s %.4d ")
+
+            return str;
+        }
+    }
+
 }
